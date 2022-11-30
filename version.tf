9d39567adf0c9fa4ae31bb9ea15767aa2e101458
ChengaDev <gazit.chen@gmail.com>
2022-11-30 09:22:46 +0200

    Update providers versions

--- version.tf
+++ version.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.2.0"
+  required_version = "~> 1.3.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.9"
+      version = "~> 4.37.0"
     }
   }
 }
