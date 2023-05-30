1213a7e28ae6bcdaaa33332d2c06c30fe8973df2
ChengaDev <gazit.chen@gmail.com>
2023-05-30 16:59:55 +0300

    cancel rename

--- modules/lambda/lambda.tf
+++ modules/lambda/lambda.tf
@@ -78,7 +78,7 @@ resource "aws_iam_policy" "secrets_iam_policy" {
   )
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_execution_role_attachment" {
+resource "aws_iam_role_policy_attachment" "lambda_execution_role" {
   role       = aws_iam_role.lambda_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
