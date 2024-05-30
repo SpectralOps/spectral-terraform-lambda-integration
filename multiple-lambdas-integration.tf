393253e50504d362cf3962f9bac893d91a770e51
chenga <gazit.chen@gmail.com>
2024-05-30 08:32:22 +0300

    formatting

--- multiple-lambdas-integration.tf
+++ multiple-lambdas-integration.tf
@@ -1,10 +1,10 @@
 module "frontend_lambda_function" {
-  count                       = local.multiple_lambda_integration ? 1 : 0
-  source                      = "./modules/lambda"
-  global_tags                 = var.global_tags
-  tags                        = var.tags
-  environment                 = var.environment
-  integration_type            = var.integration_type
+  count            = local.multiple_lambda_integration ? 1 : 0
+  source           = "./modules/lambda"
+  global_tags      = var.global_tags
+  tags             = var.tags
+  environment      = var.environment
+  integration_type = var.integration_type
   # Please do not change or replace the 'frontend' suffix since there a logic in the bot based in it
   resource_name_pattern       = "${local.resource_name_pattern}-frontend"
   env_vars                    = var.env_vars
@@ -21,12 +21,12 @@ module "frontend_lambda_function" {
 }
 
 module "backend_lambda_function" {
-  count                       = local.multiple_lambda_integration ? 1 : 0
-  source                      = "./modules/lambda"
-  global_tags                 = var.global_tags
-  tags                        = var.tags
-  environment                 = var.environment
-  integration_type            = var.integration_type
+  count            = local.multiple_lambda_integration ? 1 : 0
+  source           = "./modules/lambda"
+  global_tags      = var.global_tags
+  tags             = var.tags
+  environment      = var.environment
+  integration_type = var.integration_type
   # Please do not change or replace the 'backend' suffix since there a logic in the bot based in it
   resource_name_pattern       = "${local.resource_name_pattern}-backend"
   env_vars                    = var.env_vars
