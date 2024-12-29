9007bf3ed2f04aa25b776a9c82f2d4bfb7fc9fb7
Gavriel Hagag <gavrielh@checkpoint.com>
2024-12-29 11:52:37 +0200

    fix rejects

--- examples/vpc-in-lambda.tf
+++ examples/vpc-in-lambda.tf
@@ -4,11 +4,7 @@ module "spectral_lambda_integration" {
   integration_type = "gitlab"
 
   env_vars = {
-    # Required environment variables 
-    SPECTRAL_DSN          = "MySpectralDSN"
-    CHECK_POLICY          = "Fail on any issue" # (Fail on any issue / Fail on warnings and above / Fail on errors only / Always Pass)
-    GITLAB_ACCESS_TOKEN   = "MyGitlabToken"
-    GITLAB_WEBHOOK_SECRET = "MyGitlabWebhookSecret"
+    # Required environment variables for GitLab integration
   }
   
   # With VPC configuration
